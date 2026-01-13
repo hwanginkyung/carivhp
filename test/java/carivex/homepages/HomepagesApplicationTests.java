eec5254601c1225930c8e16aa7b4220ba7c5c11e
hik0108 <dlsrud0723@naver.com>
2026-01-13 20:22:00 +0900

    go

---
+++ test/java/carivex/homepages/HomepagesApplicationTests.java
@@ -0,0 +1,13 @@
+package carivex.homepages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
